d30f249b52f71ee9d0f078987c676512363f64e5
Marcel Arnold <mail@maesi.org>
2015-11-08 23:52:59 +0100

    add ftp upload

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+
+gulp.task('deploy', function() {
+  var remotePath = '/restftp/';
+  var conn = ftp.create({
+    host: 'ftp.rammler.ch',
+    user: args.user,
+    password: args.password,
+    parallel: 1,
+    log: gutil.log
+  });
+
+    var globs = [
+        'images/**',
+        'src/**',
+        'vendor/**',
+        '.htaccess',
+        'index.php'
+    ];
+
+  gulp.src(globs, { base: '.', buffer: false })
+    .pipe(conn.newer('restftp'))
+    .pipe(conn.dest('restftp'));
+});
